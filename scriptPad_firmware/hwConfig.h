6939e575271f9dc99691afadc00434affa902d2e
Juan <juanfloresm92@gmail.com>
2023-12-29 17:14:34 +0100

    Setting GPIO config for the matrix on latest PCB model

--- scriptPad_firmware/hwConfig.h
+++ scriptPad_firmware/hwConfig.h
@@ -1,16 +1,19 @@
 #include <map>
 
-
+// Emitting pin, switch ID, recv Pin
 std::multimap<uint8_t, std::pair<uint8_t, uint8_t>> switchMatrix = {
-    std::make_pair(18, std::make_pair(0x00, 19)),
-    std::make_pair(18, std::make_pair(0x01, 20)),
-    std::make_pair(18, std::make_pair(0x02, 21)),
-    std::make_pair(17, std::make_pair(0x03, 19)),
-    std::make_pair(17, std::make_pair(0x04, 20)),
-    std::make_pair(17, std::make_pair(0x05, 21)),
-    std::make_pair(16, std::make_pair(0x06, 19)),
-    std::make_pair(16, std::make_pair(0x07, 20)),
-    std::make_pair(16, std::make_pair(0x08, 21)),
+    std::make_pair(19, std::make_pair(1, 20)),
+    std::make_pair(18, std::make_pair(2, 20)),
+    std::make_pair(17, std::make_pair(3, 20)),
+    std::make_pair(16, std::make_pair(4, 20)),
+    std::make_pair(19, std::make_pair(5, 21)),
+    std::make_pair(18, std::make_pair(6, 21)),
+    std::make_pair(17, std::make_pair(7, 21)),
+    std::make_pair(16, std::make_pair(8, 21)),
+    std::make_pair(19, std::make_pair(9, 22)),
+    std::make_pair(18, std::make_pair(10, 22)),
+    std::make_pair(17, std::make_pair(11, 22)),
+    std::make_pair(16, std::make_pair(12, 22)),
 };
 
 
