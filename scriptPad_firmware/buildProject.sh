79eaf9731403501333692a2de6695016efa8076f
Juan <juanfloresm92@gmail.com>
2023-12-29 17:34:02 +0100

    Updating build project script

--- scriptPad_firmware/buildProject.sh
+++ scriptPad_firmware/buildProject.sh
@@ -4,5 +4,17 @@
 ./buildFrontEnd.sh
 
 #Build completed project
-cd build && make -j4
-cd ..
+
+folder_name="build"
+
+# Create build folder if it doesn't exists
+if [ ! -d "$folder_name" ]; then
+    mkdir "$folder_name"
+fi
+
+cd "$folder_name"
+
+# Build project
+if cmake ..; then
+    make -j4
+fi
